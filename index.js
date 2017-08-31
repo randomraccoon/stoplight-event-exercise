fb6c3bf283d821bd60b70e6462f198b0388719ae
Cameron Earl <developer.cameron@gmail.com>
2017-08-31 15:00:10 -0700

    Refactor code into one loop

--- index.js
+++ index.js
@@ -1,5 +1,19 @@
 (function() {
   'use strict';
 
-  // YOUR CODE HERE
+  const SPEEDS = ['stop', 'slow', 'go'];
+
+  for (let speed of SPEEDS) {
+    let btn = document.querySelector(`#${speed}Button`);
+    let light = document.querySelector(`#${speed}Light`);
+    btn.addEventListener('click', () => {
+      light.classList.toggle(speed);
+    });
+    btn.addEventListener('mouseover', () => {
+      console.log(`Entered ${speed} button`);
+    });
+    btn.addEventListener('mouseout', () => {
+      console.log(`Left ${speed} button`);
+    });
+  }
 })();
